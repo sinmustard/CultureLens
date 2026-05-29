d070d7ca573fb0cd47a54d79f7fb6a9baa0aa034
sinmustard <shinyul780308@gmail.com>
2026-05-29 19:47:22 +0900

    Add files via upload

--- CultureLens.py
+++ CultureLens.py
@@ -1,3 +1,4 @@
+import streamlit as st
 # 문화재 정보를 담고 있는 딕셔너리 - API
 culture_db = {
     "다보탑": "신라의 예술미가 돋보이는 탑입니다.",
@@ -7,13 +8,13 @@ culture_db = {
 # 문화재 설명을 찾아주는 함수
 def get_docent_info(name):
     # 입력값으로 백엔드에서 데이터 처리
-    info = culture_db[user_input]
+    info = culture_db.get[user_input]
     return info
 
 # 사용자 인터페이스
-user_input = input("궁금한 문화재 이름을 입력하세요: ")
+user_input = st.text_input("궁금한 문화재 이름을 입력하세요: ")
 
 # 함수 실행
 result = get_docent_info(user_input)
 # 출력
-print(result)
+st.write(result)
